bdfa12fe0ccd5d075f6d851e8c7324c81bccc9ae
Ваше Имя <ruslan.arkwen@gmail.com>
2026-09-24 19:29:37 +0300

    Small changes

--- src/agent_system_plots/agent.py
+++ src/agent_system_plots/agent.py
@@ -3,13 +3,13 @@ import io
 import os
 from openai import OpenAI
 
-#OLLAMA_URL = os.getenv("OLLAMA_BASE_URL", 'http://127.0.0.1:11434/v1')
+OLLAMA_URL = os.getenv("OLLAMA_BASE_URL", 'http://127.0.0.1:11434/v1')
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL", "https://openrouter.ai")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
 client = OpenAI(
     api_key='ollama',
-    base_url='http://docker.internal'
+    base_url=OLLAMA_URL
 )
 
 SYSTEM_PROMPT = '''
@@ -84,7 +84,7 @@ def extract_and_run_code(llm_output:str) -> str:
 def DA_agent(user_request:str) -> str:
     try:
         response = client.chat.completions.create(
-            model="qwen/qwen3.8-27b:free",
+            model="qwen2.5-coder:3b",
             messages=[
                 {'role':'system', 'content': SYSTEM_PROMPT},
                 {'role':'user', 'content': f'Запрос пользователя: {user_request}'}
